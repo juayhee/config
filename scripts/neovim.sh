4122a277a410373572be9c870bef1c3ae3b8028e
Juay Hee Tan <juayhee@Juays-MacBook-Air-2.local>
2023-10-18 10:05:13 +0800

    ''

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -11,27 +11,11 @@ cd ${dir}/tmp
 
 echo ">> Cloning NeoVim..."
 
-if [ $platform = "Linux" ]
-then
-	echo ">> Downloading for Linux"
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=~/.local
+make install
 
-	echo ">> Extracting archive..."
-	tar xvzf nvim-linux64.tar.gz
-	cp -r nvim-linux64/* ~/.local
-else
-	echo ">> Downloading for macOS"
-	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-macos.tar.gz
-	xattr -c ./nvim-macos.tar.gz # Avoid security check
 
-	echo ">> Extracting archive..."
-	tar xvzf nvim-macos.tar.gz
-    rm -f ~/.local/bin/nvim # For some reason running an nvim binary that overwrote
-                            # the previous binary results in 'Killed: 9'
-                            # Some kind of macOS security issue? So we remove the
-                            # old binary first
-	cp -r nvim-macos/* ~/.local
-fi
-
-rm -r ${dir}/tmp
 echo "== NeoVim installed. =="
