ac2a03579c042e7789402dacb93dbbfb2653985b
Tan Juay Hee <e0524722@u.nus.edu>
2023-10-20 12:59:37 +0800

    ''

--- config/nvim/lua/plugins/nvim-bqf.lua
+++ config/nvim/lua/plugins/nvim-bqf.lua
@@ -1,3 +1,12 @@
 return {
     'kevinhwang91/nvim-bqf',
+    config = function() 
+        require('bqf').setup({
+            func_map = {
+                prevfile = 'k',
+                nextfile = 'j',
+            }
+        })
+    end
+
 }
--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -11,11 +11,37 @@ cd ${dir}/tmp
 
 echo ">> Cloning NeoVim..."
 
-git clone https://github.com/neovim/neovim
-cd neovim
-git checkout stable
-make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=~/.local
-make install
+if [ $platform = "Linux" ]
+then
+    echo ">> Downloading for Linux"
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 
+    echo ">> Extracting archive..."
+    tar xvzf nvim-linux64.tar.gz
+    cp -r nvim-linux64/* ~/.local
+else if [ $platform = "Darwin" ]
+    echo ">> Downloading for macOS"
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-macos.tar.gz
+    xattr -c ./nvim-macos.tar.gz # Avoid security check
 
+    echo ">> Extracting archive..."
+    tar xvzf nvim-macos.tar.gz
+
+    # For some reason running an nvim binary that overwrote
+    # the previous binary results in 'Killed: 9'
+    # Some kind of macOS security issue? So we remove the
+    # old binary first
+    rm -f ~/.local/bin/nvim 
+    cp -r nvim-macos/* ~/.local
+
+else # Build from source
+    git clone https://github.com/neovim/neovim
+    cd neovim
+    git checkout stable
+    make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=~/.local
+    make install
+fi
+
+# Clean up
+rm -r ${dir}/tmp
 echo "== NeoVim installed. =="
